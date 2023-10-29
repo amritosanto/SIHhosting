53aa1d367e6cd1794c8f5e2b6a47763dd463cab5
amritosanto <61263665+amritosanto@users.noreply.github.com>
2023-10-29 19:23:56 +0530

    Delete website directory

--- website/code/verify.js
+++
@@ -1,5 +0,0 @@
-{% if result %}
-            <h1 style="color: green;">Certificate Verifed</h1>
-        {% else %}
-            <h1 style="color: red;">Certificate Invelid</h1>           
-        {% endif %}
